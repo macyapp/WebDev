a089a4a3927c3590a0f5f9f34cad36714bab3240
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-07-23 16:12:49 +0530

    ud

--- TheOdinProject/Javascript/script.js
+++ TheOdinProject/Javascript/script.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+const fruits=['apple','orange','pear'];
+
+console.log(fruits);
+
+fruits[3]='grape';
+fruits.push('mango')
+fruits.unshift('strawberry');
+
+console.log(fruits[1]);
+console.log(fruits);
