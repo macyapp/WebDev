c29c184b3388f7378030c374b345401648db8952
Sidhant Kumar Nayak <sidhantn99@gmail.com>
2023-10-08 13:50:50 +0530

    ud

--- TheOdinProject/Javascript/script.js
+++
@@ -1,6 +0,0 @@
-function showMessage(from, text) {
-  alert(from + ': ' + text);
-}
-
-showMessage('Ann', 'Hello!'); // Outputs "Ann: Hello!"
-showMessage('Ann', "What's up?"); // Outputs "Ann: What's up?"
